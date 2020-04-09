a7cc3d2f1ef8dc42e23a4348c305ef9fa2c45772
saki918 <hiro9181013@gmail.com>
2020-04-09 20:09:06 +0900

    Ⅳ終了

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -3,10 +3,30 @@ class PostController < ApplicationController
   end
 
   def index
-    @posts = Post.all.order(created_at: :desc)
+    @posts = Post.all.order(created_at: :asc)
   end
 
   def show
     @post = Post.find_by(id: params[:id])
   end
+
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    @post.save
+
+    redirect_to "/post/index"
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+
+    redirect_to '/post/index'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  root 'home/top'
-  get 'home/about'
-  get 'post/new'
-  get 'post/index'
-  get 'post/:id'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#top'
+  get '/about' => "home#about"
+
+  get 'post/new' => "post#new"
+  get 'post/index' => "post#index"
+  get 'post/:id' => 'post#show'
+  get "post/:id/edit" => 'post#edit'
+  # データベースで変更する物はpost
+  post "post/:id/update" => "post#update"
+  post 'post/:id/destroy' => 'post#destroy'
 end
